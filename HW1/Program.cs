072b92cb0244c5d7f7ac92dda4fd46c5b4bb20df
MaxYogin <yogin.eleven@gmail.com>
2022-10-01 20:56:15 +0300

    Добавил ДЗ1 -  Задача №2

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -5,18 +5,37 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
-Console.Write("Введите число A: ");
+// Console.Write("Введите число A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// if (a > b)
+// {
+// Console.WriteLine("число A больше, A = " + a);
+// }
+// else
+// {
+// Console.WriteLine("число B больше, B = " + b);
+// }
+
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число B: ");
+Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
-if (a > b)
-{
-Console.WriteLine("число A больше, A = " + a);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (b > max  & b > c ) { 
+    max = b;
+} 
+else if (c > max & c > b) { 
+    max = c; 
 }
-else
-{
-Console.WriteLine("число B больше, B = " + b);
-}
-
-
-
+Console.WriteLine ("Максимум равен " + max);
