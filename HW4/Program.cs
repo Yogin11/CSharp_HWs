4c4994f2f7ed84c14e1b241b00bc1c685c296d6b
MaxYogin <yogin11@yahoo.com>
2022-10-13 21:01:23 +0300

    ДЗ№4 задача 27

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -6,18 +6,42 @@
 2, 4 -> 16
 */
 
+// int Exponen(int a, int b)
+	// {	int d = a;
+		// for (int i = 1; i < Math.Abs(b) ; i++)
+		// {	
+			//  a = d * a;
+		// }
+		// return a;
+	// }
+	// Console.Write("Введите число А: ");
+	// int A = int.Parse(Console.ReadLine()!);
+	// Console.Write("Введите натуральное число B: ");
+	// int B = int.Parse(Console.ReadLine()!);
+    // if (B<=0) Console.WriteLine("Вы ввели не натуральное число!");
+    // else Console.WriteLine($"Число {A} в степени {B} равно  {Exponen(A, B)}");
+// 
 
-int Exponen(int a, int b)
-	{	int d = a;
-		for (int i = 1; i < Math.Abs(b) ; i++)
+/* 
+Задача 27: Напишите программу, которая принимает на вход число и 
+выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int SumDigits(int a)
 		{	
-			 a = d * a;
+			int sum = 0;
+			do
+			{
+				sum +=a%10;
+				a=a/10;
+			}
+			while (a!=0);
+		return Math.Abs(sum);
 		}
-		return a;
-	}
 	Console.Write("Введите число А: ");
-	int A = int.Parse(Console.ReadLine()!);
-	Console.Write("Введите натуральное число B: ");
-	int B = int.Parse(Console.ReadLine()!);
-    if (B<=0) Console.WriteLine("Вы ввели не натуральное число!");
-    else Console.WriteLine($"Число {A} в степени {B} равно  {Exponen(A, B)}");
+    int A = int.Parse(Console.ReadLine()!);
+	Console.WriteLine($"Сумма цифр в числе {A} равно {SumDigits(A)}");
+
+
