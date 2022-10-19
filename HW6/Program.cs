d32b2722f2f5fa07071db3c737ba101437195077
MaxYogin <yogin.eleven@gmail.com>
2022-10-19 21:12:38 +0300

    ДЗ№6 Задача 43

--- HW6/Program.cs
+++ HW6/Program.cs
@@ -17,19 +17,40 @@
 // Console.WriteLine($"Количество чисел больше нуля равно: {n}");
 
 //           Вариант 2
-Console.Clear();
-string enter = " ";
-int n = 0;
-while (enter != "s" && enter != "S")
-{
-    Console.Write($"Введите число. (Чтобы прекратить ввод чисел, наберите S): ");
-    string k = Console.ReadLine()!;
-    bool ddd = double.TryParse(k, out double d);
-    if (ddd)
-        {if (d < 0) n++;}
-    else enter = k;
-}
-Console.WriteLine();
-Console.WriteLine($"Количество чисел меньше нуля: {n}");
-Console.WriteLine();
+// Console.Clear();
+// string enter = " ";
+// int n = 0;
+// while (enter != "s" && enter != "S")
+// {
+// Console.Write($"Введите число. (Чтобы прекратить ввод чисел, наберите S): ");
+// string k = Console.ReadLine()!;
+// bool ddd = double.TryParse(k, out double d);
+// if (ddd)
+// {if (d < 0) n++;}
+// else enter = k;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Количество чисел меньше нуля: {n}");
+// Console.WriteLine();
 
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+Console.WriteLine("Даны уравнения прямых: y = k1 * x + b1 и y = k2 * x + b2");
+Console.Write("введите коэффицент k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("введите коэффицент b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("введите коэффицент k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write("введите коэффицент b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine();
+Console.WriteLine($"Координаты точки пересечения прямых: ({x:f2} ; {y:f2})");
+Console.WriteLine();
