a6e3aa6cdbb429d61a7e0de76a963108cc3fd13a
MaxYogin <yogin.eleven@gmail.com>
2022-10-19 21:03:10 +0300

    ДЗ№6 Задача 41 Вариант 2 (без М)

--- HW6/Program.cs
+++ HW6/Program.cs
@@ -3,14 +3,33 @@
 1, -7, 567, 89, 223-> 3
 */
 
-Console.Write("Введите количество чисел (М): ");
-int m = int.Parse(Console.ReadLine());
+//          Вариант 1
+// Console.Write("Введите количество чисел (М): ");
+// int m = int.Parse(Console.ReadLine());
+// 
+// int n = 0;
+// for (int i = 1; i <=m; i++)
+// {
+// Console.Write($"Введите {i}-е число: ");
+// int k = Convert.ToInt32(Console.ReadLine());
+// if (k>0) n++;
+// }
+// Console.WriteLine($"Количество чисел больше нуля равно: {n}");
 
+//           Вариант 2
+Console.Clear();
+string enter = " ";
 int n = 0;
-for (int i = 1; i <=m; i++)
+while (enter != "s" && enter != "S")
 {
-    Console.Write($"Введите {i}-е число: ");
-    int k = Convert.ToInt32(Console.ReadLine());
-    if (k>0) n++;
+    Console.Write($"Введите число. (Чтобы прекратить ввод чисел, наберите S): ");
+    string k = Console.ReadLine()!;
+    bool ddd = double.TryParse(k, out double d);
+    if (ddd)
+        {if (d < 0) n++;}
+    else enter = k;
 }
-Console.WriteLine($"Количество чисел больше нуля равно: {n}");
+Console.WriteLine();
+Console.WriteLine($"Количество чисел меньше нуля: {n}");
+Console.WriteLine();
+
