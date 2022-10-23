4a53f888287561f0b4accd2728da6ab14731fe37
MaxYogin <yogin.eleven@gmail.com>
2022-10-23 11:47:41 +0300

    ДЗ№7 задача №52

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -42,19 +42,60 @@ m = 3, n = 4.
 i = 4, j = 2 -> такого числа в массиве нет
 i = 1, j = 2 -> 2
 */
+// Console.Clear();
+// Console.Write(" Введите номер строки элемента массива: ");
+// int i = int.Parse(Console.ReadLine()!);
+// Console.Write(" Введите номер столбца элемента массива: ");
+// int j = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(" Заданный массив: ");
+// 
+// int[,] massiv = new int[5,5];
+// FillArray(5,5);
+// Console.WriteLine();
+// CheckNumber(massiv, i , j);
+// 
+// void FillArray(int m, int n){
+    // for (int i = 0; i < m ; i++) 
+    // {
+        // for (int j = 0; j < n; j++) {
+            // massiv[i,j] = new Random().Next(0,10);
+            // Console.Write(string.Format("{0,2}", massiv[i,j]));
+        // }
+        // Console.WriteLine();
+    // }
+// }
+// 
+// void CheckNumber(int[,] arr, int i, int j)
+// {   
+    // if (arr.GetLength(0)-1 < i || arr.GetLength(1)-1 < j )
+        // Console.WriteLine($" i = {i}, j = {j}  -> такого числа в массиве нет ");
+    // else        
+        // Console.WriteLine($"  i = {i}, j = {j} -> искомое число: {arr[i,j]}  "  );
+// }
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
 Console.Clear();
-Console.Write(" Введите номер строки элемента массива: ");
-int i = int.Parse(Console.ReadLine()!);
-Console.Write(" Введите номер столбца элемента массива: ");
-int j = int.Parse(Console.ReadLine()!);
-Console.WriteLine(" Заданный массив: ");
+Console.Write("Введите размерность массива m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите размерность массива n: ");
+int n = int.Parse(Console.ReadLine()!);
 
-int[,] massiv = new int[5,5];
-FillArray(5,5);
-Console.WriteLine();
-CheckNumber(massiv, i , j);
+int[,] massiv = new int[m,n];
+FillArray(m,n);
+double[] mean = ArithMean(massiv);
+Console.Write("Cреднее арифметическое каждого столбца: ");
+Console.WriteLine(String.Join("; ", mean) );
 
-void FillArray(int m, int n){
+void FillArray(int m, int n)
+{
     for (int i = 0; i < m ; i++) 
     {
         for (int j = 0; j < n; j++) {
@@ -65,16 +106,17 @@ void FillArray(int m, int n){
     }
 }
 
-void CheckNumber(int[,] arr, int i, int j)
+double[] ArithMean(int[,] arr)
 {   
-    if (arr.GetLength(0)-1 < i || arr.GetLength(1)-1 < j )
-        Console.WriteLine($" i = {i}, j = {j}  -> такого числа в массиве нет ");
-    else        
-        Console.WriteLine($"  i = {i}, j = {j} -> искомое число: {arr[i,j]}  "  );
+    double[] arith = new double[arr.GetLength(1)];  
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {   
+       double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i,j]; 
+        }
+    arith[j] = Math.Round(sum/arr.GetLength(1),1);    
+    }
+    return arith;
 }
-
-
-
-
-
-
